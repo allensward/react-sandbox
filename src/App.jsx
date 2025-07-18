92c81fd54ee5e21edf867677fef84c97b6f14dff
allenward <allen.ward@oxefit.com>
2025-07-18 16:23:37 -0500

    React JSX

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,44 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const title = 'REACT';
+const plea = 'HELP ME, ';
+const expo = 'YOU\'RE MY ONLY HOPE!';
+
+const welcome = {
+  title: title,
+  greeting: plea,
+  extra: expo
+}
+
+const dropdownOptions = [1,2,3,4,5]
+
+function getTitle(title){
+  return title;
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <h1>{welcome.greeting}{getTitle(title)}. {welcome.extra}</h1>
+        </div>
+        <div>
+          <label htmlFor='search'>Search:</label>
+          <input id='search' type='text' />
+        </div>
+        <div>
+          <select>
+            {
+              dropdownOptions.map(x => <option>{x}</option>)
+            }
+          </select>
+        </div>
+        
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
